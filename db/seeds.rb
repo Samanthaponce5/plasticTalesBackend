dd07245970bcfbfddc37f3cd0bf47622723d81b4
Samanthaponce5 <samanthaponce5@gmail.com>
2020-06-09 19:42:53 -0400

    plasticsdb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,39 +9,39 @@
 Animal.create(name:'Turtle')
 
 
-Plastic.create(name:'Plastic Info 1', info:'marine animals are killed by plastic each year', stats:100,000)
+Plastic.create(name:'Plastic Info 1', info:'marine animals are killed by plastic each year', stats:100000)
 Plastic.create(name:'Plastic Info 2', info:'tons of plastic enter the oceans every year via rivers globally', stats:1.15)
 Plastic.create(name:'Plastic Info 3', info:'turtles die each year from being tangled in plastic waste', stats:1000)
 
 
 
-Plastic.create(name: 'Straws', info:
- '500 Million straws are used by americans daily.
-  They are 1 of the top 10 items found in beach cleanups.
-  Because they are tiny and lightweight they travel long distances in rivers and sewers and end up in the sea.
-    They eventually end up in the great garbage patch.More than 10 million pounds of marine debris
-  ')
+# Plastic.create(name: 'Straws', info:
+#  '500 Million straws are used by americans daily.
+#   They are 1 of the top 10 items found in beach cleanups.
+#   Because they are tiny and lightweight they travel long distances in rivers and sewers and end up in the sea.
+#     They eventually end up in the great garbage patch.More than 10 million pounds of marine debris
+#   ')
 
-Plastic.create(name: 'Plastic Bags',
- info: 'Sea turtles can mistake clear plastic bags for one of their favorite foods which is the jellyfish.
- This can clog their digestive systems, cause them to choke. Plastic can also create pockets of air in their gut making them too bouyant so they can no longer dive for food.
- 52% of all sea turtles have eaten plastic particles
- 1 trillion plastic bags are produced worldwide in one year and every year Americans use 102.1 billion plastic bags
- 1,000 turtles die each year from being tangled in plastic waste
-')
+# Plastic.create(name: 'Plastic Bags',
+#  info: 'Sea turtles can mistake clear plastic bags for one of their favorite foods which is the jellyfish.
+#  This can clog their digestive systems, cause them to choke. Plastic can also create pockets of air in their gut making them too bouyant so they can no longer dive for food.
+#  52% of all sea turtles have eaten plastic particles
+#  1 trillion plastic bags are produced worldwide in one year and every year Americans use 102.1 billion plastic bags
+#  1,000 turtles die each year from being tangled in plastic waste
+# ')
 
-Plastic.create(name: 'Plastic bottles', info: '
-Americans consume 8.6 billion gallons of bottled water anually
-Only 1 in 5 bottles are recycled. The other 4 contribute to the 3 billion pounds of plastic water bottles added to land fill each year.
-80% of water bottles are not recycled
-92%of seabirds found in a test area ingested plastic. Bottle caps are not recycled
-On average per person approximately 185 lbs of plastic is thrown away per year.
-')
+# Plastic.create(name: 'Plastic bottles', info: '
+# Americans consume 8.6 billion gallons of bottled water anually
+# Only 1 in 5 bottles are recycled. The other 4 contribute to the 3 billion pounds of plastic water bottles added to land fill each year.
+# 80% of water bottles are not recycled
+# 92%of seabirds found in a test area ingested plastic. Bottle caps are not recycled
+# On average per person approximately 185 lbs of plastic is thrown away per year.
+# ')
 
-Plastic.create(name: '?', info: '500 Million')
+# Plastic.create(name: '?', info: '500 Million')
 
-Plastic.create(name: 'Styrofoam', info: "It's everywhere!! 25 billion plastic foam coffe cups alone are thrown away each year.
-Plastic foam is the most harmful material for the environment.
-Less than 1% of all plastic foam is recycled.
-An estimated 4 million to 12 million tonnes of plastic enter Earth’s oceans every year.")
+# Plastic.create(name: 'Styrofoam', info: "It's everywhere!! 25 billion plastic foam coffe cups alone are thrown away each year.
+# Plastic foam is the most harmful material for the environment.
+# Less than 1% of all plastic foam is recycled.
+# An estimated 4 million to 12 million tonnes of plastic enter Earth’s oceans every year.")
 
